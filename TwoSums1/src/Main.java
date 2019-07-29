6b0552ecf96a3e2ff761aebac8ca812a37f5e4b1
Minh <s3722599@student.rmit.edu.au>
2019-07-29 18:28:38 +1000

    first commit

---
+++ TwoSums1/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Main {
+	
+    public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+    	int INIT = 0;
+        
+		for (int i = INIT; i < nums.length; i++) {
+			int sec = i + 1;
+			for(int j = sec; j < nums.length; j++) {
+				if(nums[i] + nums[j] == target) {
+					answer[0] = nums[i];
+					answer[1] = nums[j];
+					//exits
+					i = nums.length;
+					j = nums.length;
+				}
+			}
+		}	
+		return answer;
+    }
+
+    
+    public void run() {
+    	int[] ar = {1,2,3,4,5,6,7,8,9};
+    	System.out.println(Arrays.toString(twoSum(ar,9)));
+    }
+	
+}
+
