5e71569d3ee104dbecf49663d1a572a6e21d1197
Minh <s3722599@student.rmit.edu.au>
2019-08-03 01:16:38 +1000

    basic functionality done of s2

--- LongestSubsNoRep/src/Main.java
+++ LongestSubsNoRep/src/Main.java
@@ -1,8 +1,10 @@
 
 public class Main {
 
-	
-	
+/*
+ * 	Solution1: when the longest substr is at the starting index of 0
+ */
+
 //    public int lengthOfLongestSubstring(String s) {
 //       String str = ""; 
 //       boolean done = false;
@@ -24,30 +26,40 @@ public class Main {
 //       return str.length();
 //    }
     
+	
+	//solution2
     public int lengthOfLongestSubstring(String s) {
-        String str = ""; 
-        boolean done = false;
-        for(int i = 0; i < s.length(); i++) {
-     	   if(!done) {
-          	   char a = s.charAt(i);
-
-         	   for(int j = 0; j < i; j++) {
-              	   if(a == s.charAt(j)) {
-             		   done = true;
+    	String longestStr = "";
+        for(int k = 0; k < s.length(); k++) {
+            boolean done = false;
+            String str = "";
+            for(int i = k; i < s.length(); i++) {
+         	   if(!done) {
+              	   char a = s.charAt(i);
+             	   for(int j = k; j < i; j++) {
+                  	   if(a == s.charAt(j)) {
+                 		   done = true;
+                 	   }
+             	   }
+             	   if(!done) {
+             		   str += a;
              	   }
          	   }
-         	   if(!done) {
-         		   str += a;
-         	   }
-     	   }
+            }
+            if(longestStr.length() < str.length()) {
+            	longestStr = str;
+            }
+//            if(longestStr.length() < s.length()/2) {
+//            	
+//            }
         }
-        System.out.println(str);
-        return str.length();
+        System.out.println(longestStr);
+        return longestStr.length();
      }
     
 	public void run() {
 		// TODO Auto-generated method stub
-		int len = this.lengthOfLongestSubstring("abcda");
+		int len = this.lengthOfLongestSubstring("abcabcbb");
 		System.out.println(len);
 	}
 
