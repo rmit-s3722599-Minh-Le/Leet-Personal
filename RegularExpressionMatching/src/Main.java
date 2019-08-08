592f09bc66aad288b8b629e065bf3c7e97f0b032
Minh <s3722599@student.rmit.edu.au>
2019-08-08 22:44:32 +1000

    first commit, skeleton and partial pseudo code

---
+++ RegularExpressionMatching/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+	
+	//getting started
+    public boolean isMatch(String s, String p) {
+        boolean idkyet = false;
+        int token;
+        //predict the future
+        //if the next passage is a *
+        //then skip the current section i.e (x*), where x is a variable of a char
+        //check to see if the next section variable is the same as x
+        //if so, then prioritise that variable over x
+        for(int i = 0; i < p.length(); i++) {
+            
+            char a = p.charAt(i);
+            // if(s.charAt(i));
+            
+        }
+        return idkyet;
+    }
+
+	public void run() {
+		
+	}
+
+}
